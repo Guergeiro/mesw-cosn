22093dd93fb0e570cd64535d572ec29fa9e9df65
diogofalken <diogofalken0@gmail.com>
2022-12-11 16:05:15 +0000

    feat: add faculty DB, repository and logic to degrees service

--- db/degrees.sql
+++ db/degrees.sql
@@ -13,3 +13,13 @@ create table degrees (
     "abbr" text,
     primary key (id)
 );
+
+drop table if exists faculties;
+
+create table faculties (
+    "id" uuid default gen_random_uuid(),
+    primary key (id)
+);
+
+insert into faculties (id)
+values ('73e12576-04e0-465d-9ba8-1c3c8bcd9d54'), ('78420303-653c-417f-a397-b9fc7e441975');
